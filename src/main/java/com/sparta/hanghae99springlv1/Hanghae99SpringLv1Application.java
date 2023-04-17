a9629597302831b44c9faebb3f224fd09b6b98e5
InyongHwang <123296558+InyongHwang@users.noreply.github.com>
2023-04-17 13:20:03 +0900

    Start Spring Project
    스프링 프로젝트 기본 파일 업로드

---
+++ src/main/java/com/sparta/hanghae99springlv1/Hanghae99SpringLv1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghae99springlv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99SpringLv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hanghae99SpringLv1Application.class, args);
+	}
+
+}
